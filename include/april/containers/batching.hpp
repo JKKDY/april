8fe86d278cca328b65c088db506f5f30484d1188
Julian <jdymail99@gmail.com>
2026-01-28 01:32:57 +0100

    perf: add chunked batches featuring chunk based iteration and pre fetching

--- include/april/containers/batching.hpp
+++ include/april/containers/batching.hpp
@@ -158,5 +158,159 @@ namespace april::batching {
 		Container & container;
 	};
 
+
+
+	template<typename Container, typename ChunkPtr>
+	struct AsymmetricChunkedBatch : SerialBatch {
+		explicit AsymmetricChunkedBatch(Container & container, ChunkPtr * chunks): container(container), chunks(chunks) {}
+
+		template<env::FieldMask Mask, typename Func>
+		AP_FORCE_INLINE void for_each_pair (Func && f) const {
+			// skip empty range
+		    if (range1_chunks.start == range1_chunks.end || range2_chunks.start == range2_chunks.end) return;
+
+		    constexpr size_t stride = Container::chunk_size;
+
+			// peel of last chunk (i.e. the tail)
+		    const size_t c1_body_end = range1_chunks.end - 1;
+		    const size_t c2_body_end = range2_chunks.end - 1;
+
+		    // if tail is 0, it means the chunk is actually full (stride)
+		    const size_t limit1_tail = (range1_tail == 0) ? stride : range1_tail;
+		    const size_t limit2_tail = (range2_tail == 0) ? stride : range2_tail;
+
+		    // body vs body with hardcoded stride x stride loop
+		    for (size_t c1 = range1_chunks.start; c1 < c1_body_end; ++c1) {
+		        AP_PREFETCH(chunks + c1 + 1);
+
+		        for (size_t c2 = range2_chunks.start; c2 < c2_body_end; ++c2) {
+		            AP_PREFETCH(chunks + c2 + 1);
+
+		            for (size_t i = 0; i < stride; ++i) {
+		                auto p1 = container.template restricted_at<Mask>(c1, i);
+		                for (size_t j = 0; j < stride; ++j) {
+		                    auto p2 = container.template restricted_at<Mask>(c2, j);
+		                    f(p1, p2);
+		                }
+		            }
+		        }
+		    }
+
+		    // body 1 vs tail 2 (iterate full chunks of R1 against the single partial chunk of R2)
+		    for (size_t c1 = range1_chunks.start; c1 < c1_body_end; ++c1) {
+		    	AP_PREFETCH(chunks + c1 + 1);
+		        for (size_t i = 0; i < stride; ++i) {
+		            auto p1 = container.template restricted_at<Mask>(c1, i);
+		            for (size_t j = 0; j < limit2_tail; ++j) {
+		                auto p2 = container.template restricted_at<Mask>(c2_body_end, j);
+		                f(p1, p2);
+		            }
+		        }
+		    }
+
+			// body 2 vs tail 1 (iterate full chunks of R2 against the single partial chunk of R1
+		    for (size_t c2 = range2_chunks.start; c2 < c2_body_end; ++c2) {
+		    	AP_PREFETCH(chunks + c2 + 1);
+		        for (size_t i = 0; i < limit1_tail; ++i) {
+		            auto p1 = container.template restricted_at<Mask>(c1_body_end, i);
+		            for (size_t j = 0; j < stride; ++j) {
+		                auto p2 = container.template restricted_at<Mask>(c2, j);
+		                f(p1, p2);
+		            }
+		        }
+		    }
+
+		    // tail 1 vs tail 2 (Interaction between the two last chunks)
+	        for (size_t i = 0; i < limit1_tail; ++i) {
+	            auto p1 = container.template restricted_at<Mask>(c1_body_end, i);
+	            for (size_t j = 0; j < limit2_tail; ++j) {
+	                auto p2 = container.template restricted_at<Mask>(c2_body_end, j);
+	                f(p1, p2);
+	            }
+	        }
+		}
+
+		// Ranges represent chunk indices! (e.g., 0 to 4 means Chunks 0,1,2,3)
+		Range range1_chunks; // Chunk Indices [start, end)
+		size_t range1_tail{};  // Number of valid items in the last chunk of range1 (0 = Full)
+
+		Range range2_chunks;
+		size_t range2_tail{};
+	private:
+		Container & container;
+		ChunkPtr * AP_RESTRICT chunks = container.ptr_chunks;
+	};
+
+
+	template<typename Container, typename ChunkPtr>
+	struct SymmetricChunkedBatch : SerialBatch {
+		explicit SymmetricChunkedBatch(Container & container, ChunkPtr * chunks) : container(container), chunks(chunks) {}
+
+
+		template<env::FieldMask Mask, typename Func>
+	    AP_FORCE_INLINE void for_each_pair (Func && f) const {
+	        if (range_chunks.start == range_chunks.end) return;
+	        constexpr size_t stride = Container::chunk_size;
+
+			// peel of last chunk (i.e. the tail)
+	        const size_t c_body_end = range_chunks.end - 1;
+	        const size_t limit_tail = (range_tail == 0) ? stride : range_tail;
+
+	        // body (iterate c1 up to the last full chunk)
+	        for (size_t c1 = range_chunks.start; c1 < c_body_end; ++c1) {
+	            AP_PREFETCH(chunks + c1 + 1);
+
+	            // chunk self interaction
+	            for (size_t i = 0; i < stride; ++i) {
+	                auto p1 = container.template restricted_at<Mask>(c1, i);
+	                for (size_t j = i + 1; j < stride; ++j) {
+	                     auto p2 = container.template restricted_at<Mask>(c1, j);
+	                     f(p1, p2);
+	                }
+	            }
+
+	        	// interaction with all other loops
+	            for (size_t c2 = c1 + 1; c2 < c_body_end; ++c2) {
+	                AP_PREFETCH(chunks + c2 + 1);
+	                for (size_t i = 0; i < stride; ++i) {
+	                    auto p1 = container.template restricted_at<Mask>(c1, i);
+	                    for (size_t j = 0; j < stride; ++j) {
+	                        auto p2 = container.template restricted_at<Mask>(c2, j);
+	                        f(p1, p2);
+	                    }
+	                }
+	            }
+	        }
+
+	        // body vs tail (every body chunk with tail chunk)
+	        for (size_t c1 = range_chunks.start; c1 < c_body_end; ++c1) {
+	            AP_PREFETCH(chunks + c1 + 1);
+	            for (size_t i = 0; i < stride; ++i) {
+	                auto p1 = container.template restricted_at<Mask>(c1, i);
+	                for (size_t j = 0; j < limit_tail; ++j) {
+	                     auto p2 = container.template restricted_at<Mask>(c_body_end, j);
+	                     f(p1, p2);
+	                }
+	            }
+	        }
+
+	        // tail (interact tail chunk with itself)
+            for (size_t i = 0; i < limit_tail; ++i) {
+                auto p1 = container.template restricted_at<Mask>(c_body_end, i);
+                for (size_t j = i + 1; j < limit_tail; ++j) {
+                     auto p2 = container.template restricted_at<Mask>(c_body_end, j);
+                     f(p1, p2);
+                }
+            }
+	    }
+
+		// Range represents chunk indices! (e.g., 0 to 4 means Chunks 0,1,2,3)
+		Range  range_chunks;  // Chunk Indices [start, end)
+		size_t range_tail{};  // Number of valid items in the last chunk of range1 (0 = Full)
+	private:
+		Container & container;
+		ChunkPtr * AP_RESTRICT const chunks = container.ptr_chunks;
+	};
+
 }
 
--- include/april/containers/direct_sum/aosoa.hpp
+++ include/april/containers/direct_sum/aosoa.hpp
@@ -11,40 +11,43 @@ namespace april::container::direct_sum {
     public:
         using Base = DirectSumCore<layout::AoSoA<Config, U>>;
         using Base::chunk_size;
-        using SymmetricBatch = batching::SymmetricScalarBatch<DirectSumAoSoAImpl>;
-        using AsymmetricBatch = batching::AsymmetricScalarBatch<DirectSumAoSoAImpl>;
+        using SymmetricBatch = batching::SymmetricChunkedBatch<DirectSumAoSoAImpl, typename Base::Chunk>;
+        using AsymmetricBatch = batching::AsymmetricChunkedBatch<DirectSumAoSoAImpl, typename Base::Chunk>;
 
         using Base::Base;
         friend Base;
 
         void generate_batches(const std::vector<std::pair<size_t, size_t>> &) {
-            const auto n_types = static_cast<env::ParticleType>(this->bin_counts.size());
+            const auto n_types = static_cast<env::ParticleType>(this->bin_starts.size() - 1); // bin_starts has N+1 entries
 
-            // build symmetric batches
             for (env::ParticleType type = 0; type < n_types; type++) {
-                // Use the new helper to get PHYSICAL indices
-                auto [start, end] = this->get_physical_bin_range(type);
+                auto [chunk_start, chunk_end] = this->get_chunk_bin_range(type);
 
-                if (start == end) continue;
+                if (chunk_start == chunk_end) continue;
 
-                SymmetricBatch batch (*this);
+                SymmetricBatch batch (*this,  this->ptr_chunks);
                 batch.types = {type, type};
-                batch.range = {start, end};
+                batch.range_chunks = {chunk_start, chunk_end};
+                batch.range_tail = this->bin_sizes[type] % chunk_size;
+
                 symmetric_batches.push_back(batch);
             }
 
-            // build symmetric batches
+            // build asymmetric batches
             for (env::ParticleType t1 = 0; t1 < n_types; t1++) {
                 for (env::ParticleType t2 = t1 + 1; t2 < n_types; t2++) {
-                    auto [start1, end1] = this->get_physical_bin_range(t1);
-                    auto [start2, end2] = this->get_physical_bin_range(t2);
+                    auto [c1_start, c1_end] = this->get_chunk_bin_range(t1);
+                    auto [c2_start, c2_end] = this->get_chunk_bin_range(t2);
 
-                    if (start1 == end1 || start2 == end2) continue;
+                    if (c1_start == c1_end || c2_start == c2_end) continue;
 
-                    AsymmetricBatch batch (*this);
+                    AsymmetricBatch batch (*this, this->ptr_chunks);
                     batch.types = {t1, t2};
-                    batch.range1 = {start1, end1};
-                    batch.range2 = {start2, end2};
+                    batch.range1_chunks = {c1_start, c1_end};
+                    batch.range2_chunks = {c2_start, c2_end};
+                    batch.range1_tail = this->bin_sizes[t1] % chunk_size;
+                    batch.range2_tail = this->bin_sizes[t2] % chunk_size;
+
                     asymmetric_batches.push_back(batch);
                 }
             }

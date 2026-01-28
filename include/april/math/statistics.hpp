170fd0ff348bdc2aa93244f9a64a8ec27fdc3b4e
Julian <jdymail99@gmail.com>
2026-01-28 17:20:29 +0100

    refac: make maxwell_boltzmann_velocity distribution thread safe

--- include/april/math/statistics.hpp
+++ include/april/math/statistics.hpp
@@ -1,24 +1,29 @@
 #pragma once
-
 #include <random>
-
 #include "april/base/types.hpp"
 
-// we use a constant seed for repeatability.
-// random engine needs static lifetime otherwise it would be recreated for every call.
-static std::default_random_engine randomEngine(42);
+namespace april::math {
 
-namespace april::shared {
+	// Helper to get a thread-safe, consistent engine
+	inline std::default_random_engine& get_random_engine() {
+		thread_local std::default_random_engine engine(42);
+		return engine;
+	}
 
-	inline vec3 maxwell_boltzmann_velocity_distribution(const double averageVelocity, const size_t dimensions) {
-		// when adding independent normally distributed values to all velocity components
-		// the velocity change is maxwell boltzmann distributed
-		std::normal_distribution<double> normalDistribution{0, 1};
-		vec3 randomVelocity{};
-		for (size_t  i = 0; i < dimensions; ++i) {
-			randomVelocity[static_cast<int>(i)] = averageVelocity * normalDistribution(randomEngine);
+	/**
+	 * Generates a velocity vector according to the Maxwell-Boltzmann distribution.
+	 * @param averageVelocity Scaling factor (related to sqrt(kT/m))
+	 * @param dimensions Number of active dimensions (1, 2, or 3)
+	 */
+	inline vec3d maxwell_boltzmann_velocity(const double averageVelocity, const size_t dimensions = 3) {
+		std::normal_distribution dist{0.0, 1.0};
+		auto& engine = get_random_engine();
+
+		vec3d velocity{0.0};
+		for (size_t i = 0; i < dimensions; ++i) {
+			velocity[static_cast<int>(i)] = averageVelocity * dist(engine);
 		}
-		return randomVelocity;
+		return velocity;
 	}
 
 }

63418b3c7756b8295575611185406ebe65915588
Julian <jdymail99@gmail.com>
2026-08-13 15:40:41 +0200

    fix reduce min/max duplication in backend_std_simd.hpp

--- include/april/simd/backends/backend_std_simd.hpp
+++ include/april/simd/backends/backend_std_simd.hpp
@@ -1211,23 +1211,13 @@ namespace april::simd::internal::std_simd {
 				return Packed{stdx::abs(x.data)};
 		}
 
-    	[[nodiscard]] storage_type reduce_min() const noexcept {
-        	return stdx::reduce(data, [](const auto& a, const auto& b) {
-				if constexpr (requires { stdx::min(a, b); })
-					return stdx::min(a, b);
-				else
-					return std::min(a, b);
-			});
-        }
+		[[nodiscard]] static Packed min(const Packed& a, const Packed& b) {
+			return Packed{stdx::min(a.data, b.data)};
+		}
 
-    	[[nodiscard]] storage_type reduce_max() const noexcept {
-        	return stdx::reduce(data, [](const auto& a, const auto& b) {
-				if constexpr (requires { stdx::max(a, b); })
-					return stdx::max(a, b);
-				else
-					return std::max(a, b);
-			});
-        }
+		[[nodiscard]] static Packed max(const Packed& a, const Packed& b) {
+			return Packed{stdx::max(a.data, b.data)};
+		}
 
 		[[nodiscard]] static Packed clamp(
 			const Packed& x,
@@ -1513,22 +1503,23 @@ namespace april::simd::internal::std_simd {
         	return static_cast<storage_type>(stdx::reduce(data));
         }
 
-    	[[nodiscard]] storage_type reduce_min() const {
-        	return static_cast<storage_type>(
-				stdx::reduce(data, [](const auto& a, const auto& b) {
+    	[[nodiscard]] storage_type reduce_min() const noexcept {
+        	return stdx::reduce(data, [](const auto& a, const auto& b) {
+				if constexpr (requires { stdx::min(a, b); })
+					return stdx::min(a, b);
+				else
 					return std::min(a, b);
-				})
-			);
+			});
         }
 
-    	[[nodiscard]] storage_type reduce_max() const {
-        	return static_cast<storage_type>(
-				stdx::reduce(data, [](const auto& a, const auto& b) {
+    	[[nodiscard]] storage_type reduce_max() const noexcept {
+        	return stdx::reduce(data, [](const auto& a, const auto& b) {
+				if constexpr (requires { stdx::max(a, b); })
+					return stdx::max(a, b);
+				else
 					return std::max(a, b);
-				})
-			);
+			});
         }
-
 
     	// ------------------------
     	// Debugging and inspection
